394ec2b0c927b7fbec0a22eccd58609acebc7e61
mchcm-13579 <124559829+mchcm-13579@users.noreply.github.com>
2023-05-28 20:01:16 +0800

    Update submit_message.php

--- zheng_huo/submit_message.php
+++ zheng_huo/submit_message.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $message = $_POST['message'];
-  $filename = 'messages.txt'; // 指定保存留言的文件名
-  $file = fopen($filename, 'a'); // 打开文件并以追加方式写入
-  fwrite($file, $message . PHP_EOL); // 写入留言信息
-  fclose($file); // 关闭文件句柄
-  // 提示用户提交成功
+  $filename = 'messages.txt'; 
+  $file = fopen($filename, 'a'); 
+  fwrite($file, $message . PHP_EOL); 
+  fclose($file); 
+  
   echo "<script>alert('提交成功！'); window.history.go(-1);</script>";
   exit();
 }
